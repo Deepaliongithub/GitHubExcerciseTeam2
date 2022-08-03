ab67367000f6920c884a40eb93d0bc9df63e79c1
Varsha <varsha.kalgi@gmail.com>
2022-08-03 10:23:56 +0530

    First Commit

---
+++ src/com/Fujitsu/FindAreaNPerimeter.java
@@ -0,0 +1,28 @@
+package com.Fujitsu;
+
+import java.util.Scanner;
+
+public class FindAreaNPerimeter {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("1.Area of a Rectangle");
+		System.out.println("2.Area of a Triangle");
+		System.out.println("3.Area of a Circle");
+		System.out.println("4.Perimeter of a Rectangle");
+		System.out.println("5.Perimeter of a Trianle ");
+		System.out.println("6.Area of a cube "); 
+		System.out.println("Enter your choice ");
+		int choice=sc.nextInt();
+		switch(choice)
+		{
+		case 1:
+	
+			break;
+		case 2:
+			break;
+		}
+	}
+
+}
